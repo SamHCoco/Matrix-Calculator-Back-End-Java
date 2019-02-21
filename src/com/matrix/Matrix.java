0c0de06bd5cfdb683b8250be3691316597b2ccb7
SamHCoco <42847052+SamHCoco@users.noreply.github.com>
2019-02-21 23:39:25 +0000

    Modifications to 2x2 and 3x3 Determinant methods made

--- src/com/matrix/Matrix.java
+++ src/com/matrix/Matrix.java
@@ -1,11 +1,12 @@
 package com.matrix;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Matrix {
     private int rowInput; // user input for number of rows
     private int columnInput; // user input for number of columns
-    private int[][] createdMatrix; // the addedMatrix to be created from user input values
+    private int[][] matrix; // the addedMatrix to be created from user input values
     private String name; // The name of the addedMatrix e.g 'A', 'B' etc
     private double matrixDeterminant;
 
@@ -25,11 +26,11 @@ public class Matrix {
             columnInput = scanner.nextInt(); // Number of columns
 
             // Creates addedMatrix and asks user to input values of addedMatrix row by row
-            this.createdMatrix = new int[rowInput][columnInput]; // size of addedMatrix based on user input
+            this.matrix = new int[rowInput][columnInput]; // size of addedMatrix based on user input
             System.out.println("Enter the values of Matrix " + this.name + " row by row:");
-            for(int i = 0; i < createdMatrix.length; i++){
-                for(int j = 0; j< createdMatrix[rowInput - 1].length; j++){
-                        createdMatrix[i][j] = scanner.nextInt();
+            for(int i = 0; i < matrix.length; i++){
+                for(int j = 0; j< matrix[rowInput - 1].length; j++){
+                        matrix[i][j] = scanner.nextInt();
                 }
             }
         }
@@ -40,9 +41,9 @@ public class Matrix {
     // DISPLAY USER SPECIFIED MATRIX TO USER
     public void getMatrix(){
         System.out.println("Matrix " + name + ": ");
-        for(int i=0; i < createdMatrix.length; i++){
-            for(int j=0; j < createdMatrix[rowInput - 1].length; j++){ // [rowInput - 1] since index starts at 0
-                System.out.print(createdMatrix[i][j] + "\t");
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[rowInput - 1].length; j++){ // [rowInput - 1] since index starts at 0
+                System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
         }
@@ -55,10 +56,10 @@ public class Matrix {
     public static void addMatrices(Matrix A, Matrix B){
         int[][] addedMatrix; // stores the result of addedMatrix addition
         if(A.rowInput == B.rowInput && A.columnInput == B.columnInput){
-            addedMatrix = new int[A.createdMatrix.length][B.createdMatrix.length]; // Will contain final result of calculation
-            for(int i = 0; i < A.createdMatrix.length; i++){
-                for(int j = 0; j < A.createdMatrix.length; j++){
-                    addedMatrix[i][j] = A.createdMatrix[i][j] + B.createdMatrix[i][j];
+            addedMatrix = new int[A.matrix.length][B.matrix.length]; // Will contain final result of calculation
+            for(int i = 0; i < A.matrix.length; i++){
+                for(int j = 0; j < A.matrix.length; j++){
+                    addedMatrix[i][j] = A.matrix[i][j] + B.matrix[i][j];
                 }
             }
             System.out.println("ADDED MATRIX");
@@ -74,10 +75,10 @@ public class Matrix {
     public static void subtractMatrices(Matrix a, Matrix b){
         int[][] subtractedMatrix; // stores the result of addedMatrix subtraction
         if(a.rowInput == b.rowInput && a.columnInput == b.columnInput){
-            subtractedMatrix  = new int[a.createdMatrix.length][b.createdMatrix.length]; // Will contain final result of calculation
-            for(int i = 0; i < a.createdMatrix.length; i++){
-                for(int j = 0; j < a.createdMatrix.length; j++){
-                    subtractedMatrix[i][j] = a.createdMatrix[i][j] - b.createdMatrix[i][j];
+            subtractedMatrix  = new int[a.matrix.length][b.matrix.length]; // Will contain final result of calculation
+            for(int i = 0; i < a.matrix.length; i++){
+                for(int j = 0; j < a.matrix.length; j++){
+                    subtractedMatrix[i][j] = a.matrix[i][j] - b.matrix[i][j];
                 }
             }
             // DISPLAYS ADDED MATRIX TO THE USER
@@ -100,42 +101,57 @@ public class Matrix {
     /*
     Calculates the determinant of a addedMatrix
      */
-    public void calculateDeterminant(Matrix a){
+    public void calculateDeterminant(){
+        int whichDeterminant = findMatrixSize();
+        switch(whichDeterminant){
+            case 2:
+                calculate2x2Det();
+                break;
+        }
 
     }
 
     // this method will return a double (this return will be needed for 3x3 determinants)
-    public void calculate2x2Det(int[][] m){
-        matrixDeterminant = m[1][1] * m[0][0] - m[1][0] * m[0][1];
+    public void calculate2x2Det(){
+        matrixDeterminant = matrix[1][1] * matrix[0][0] - matrix[1][0] * matrix[0][1];
     }
 
 
-    public void calculate3x3Det(int[][] m){
-
+    public int findMatrixSize(){
+        if(rowInput == 2 && columnInput == 2){
+            return 2;
+        } else if(rowInput == 3 && columnInput == 3){
+            return 3;
+        }
+        else {
+            return -1;
+        }
     }
 
     public void extract2x2Matrix(){
         int iteration = 1;
-        int[][] extractedMatrix = new int[2][2];
+        ArrayList<Integer[][]> cofactorMatrices = new ArrayList<Integer[][]>();
         System.out.println("\nEXTRACTED FROM MATRIX: " + name);
         do{
-            for(int i = 0; i < createdMatrix.length - 1; i++ ){
-                for(int j = 0; j < createdMatrix.length - 1; j++){
+            Integer[][] extractedMatrix = new Integer[2][2];
+            for(int i = 0; i < matrix.length - 1; i++ ){
+                for(int j = 0; j < matrix.length - 1; j++){
                     if(iteration == 1){
-                        System.out.print(createdMatrix[i+1][j+1] + "\t");
+                       extractedMatrix[i][j] = matrix[i+1][j+1];
                     }else if(iteration == 2){
                         if(j == 1){
-                            System.out.print(createdMatrix[i+1][j+1] + "\t");
+                            System.out.print(matrix[i+1][j+1] + "\t");
                         } else {
-                            System.out.print(createdMatrix[i+1][j] + "\t");
+                            System.out.print(matrix[i+1][j] + "\t");
                         }
                     } else if(iteration == 3){
-                        System.out.print(createdMatrix[i+1][j] + "\t");
+                        System.out.print(matrix[i+1][j] + "\t");
                     }
                 }
                 System.out.println();
             }
             System.out.println();
+            cofactorMatrices.add(extractedMatrix);
             iteration++;
         } while(iteration <= 3 );
     }
